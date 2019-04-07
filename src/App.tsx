f1fdadd06d9305515d1e27ac1f4b0c01b9c40e8f
Glendon Philipp Baculio <gpbaculio@gmail.com>
2019-04-08 01:03:04 +0800

    added design

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,81 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, { Component, useState } from 'react';
+import Toggle from 'react-toggle';
 import './App.css';
+import 'react-toggle/style.css';
 
-class App extends Component {
+interface AppProps {}
+interface AppState {
+  [key: string]: boolean;
+}
+class App extends Component<AppProps, AppState> {
+  state = {
+    power: false,
+    bank: false
+  };
+  handleToggle = (key: string) => {
+    const { key: val }: any = this.state;
+    this.setState({ [key]: val });
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.tsx</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className='App d-flex justify-content-center align-items-center'>
+        <div className='drum-container d-flex'>
+          <div className='pad-container w-50 d-flex flex-column'>
+            <div className='d-flex'>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                Q
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                W
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                E
+              </div>
+            </div>
+            <div className='d-flex'>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                A
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                S
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                D
+              </div>
+            </div>
+            <div className='d-flex'>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                Z
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                X
+              </div>
+              <div className='pad m-3 d-flex justify-content-center align-items-center'>
+                C
+              </div>
+            </div>
+          </div>
+          <div className='controls-container w-50 d-flex flex-column justify-content-between align-items-center'>
+            <div className='d-flex flex-column'>
+              <div className='text-center'>Power</div>
+              <Toggle
+                defaultChecked={this.state.power}
+                icons={false}
+                onChange={() => this.handleToggle('power')}
+              />
+            </div>
+            <div className='mode'>Mode</div>
+            <input type='range' />
+            <div className='d-flex flex-column'>
+              <div className='text-center'>Bank</div>
+              <Toggle
+                defaultChecked={this.state.bank}
+                icons={false}
+                onChange={() => this.handleToggle('bank')}
+              />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
